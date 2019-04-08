87ce36c2545ecfbdcea8783ff2e8a4de4a66fd63
Karl Jones <karl@unity3d.com>
2019-04-08 21:17:07 +0100

    Converted sgi texture reader into a package.

---
+++ com.unity.images.sgi/Editor/TextureSgiImporter.cs
@@ -0,0 +1,16 @@
+﻿using Unity.Images.SGI;
+using UnityEditor.Experimental.AssetImporters;
+
+namespace UnityEditor.Images.SGI
+{
+    [ScriptedImporter(1, new string[] { "sgi", "rgb", "rgba", "bw", "int", "inta" })]
+    public class TextureSgiImporter : ScriptedImporter
+    {
+        public override void OnImportAsset(AssetImportContext ctx)
+        {
+            var image = new TextureSgi(ctx.assetPath);
+            ctx.AddObjectToAsset("Texture", image.Texture);
+            ctx.SetMainObject(image.Texture);
+        }
+    }
+}
